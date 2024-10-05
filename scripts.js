57a0caa05609df01421f1c750e3d7df01d282536
Julian-M-87 <julianmartina87@gmail.com>
2024-10-04 23:07:21 -0300

    513

---
+++ scripts.js
@@ -0,0 +1,112 @@
+let video = $('video')[0]
+let botonPlay = $('#play')
+let botonPausa = $('#pausa')
+let botonReiniciar = $('#reiniciar')
+let ventana = $(window)
+let contador = 0
+
+
+ventana.on('scroll', function () {
+    if (ventana.height() * 0.5 < ventana.scrollTop()) {
+        $('nav').addClass('fondoNav');
+    } else {
+        $('nav').removeClass('fondoNav');
+    }
+});
+
+function mostrarTiempoVideo(tiempo) {
+    if (tiempo < 60) {
+        if (tiempo.toFixed(0) < 10) {
+            return `00:0${tiempo.toFixed(0)}`
+        }
+        return `00:${tiempo.toFixed(0)}`
+    } else {
+        let minutos = parseInt(tiempo / 60)
+        let segundos = (tiempo / 60 - minutos)
+        if (segundos < 10) {
+            return `${minutos}:${segundos.toFixed(0)}`
+        }
+        return `${minutos}:${segundos.toFixed(0)}`
+    }
+}
+
+let tiempoTranscurrido;
+
+setTimeout(function () {
+    $('#tiempoVideo').html("Duracion video 04:41")
+}, 100);
+
+botonPlay.on('click', function () {
+    video.play()
+    tiempoTranscurrido = setInterval(function () {
+        $('#tiempoVideo').html(`${mostrarTiempoVideo(video.currentTime)}`)
+    }, 1000)
+})
+
+botonPausa.on('click', function () {
+    video.pause()
+    clearInterval(mostrarTiempoVideo)
+})
+
+botonReiniciar.on('click', reiniciar)
+
+function inicio() {
+    var imagenes = $('#img-box img')
+    soltarImg = $('#drop-box')
+    soltarImg2 = $('#drop-box2')
+    soltarImg3 = $('#drop-box3')
+    console.log(imagenes.length)
+    console.log(imagenes)
+
+
+    imagenes.each(function () {
+        $(this).on('dragstart', arrastrar);
+    });
+
+    soltarImg.on('dragenter', function (event) {
+        event.preventDefault();
+    });
+    soltarImg.on('dragover', function (event) {
+        event.preventDefault();
+    });
+    soltarImg.on('drop', soltar);
+
+
+    soltarImg2.on('dragenter', function (event) {
+        event.preventDefault();
+    });
+    soltarImg2.on('dragover', function (event) {
+        event.preventDefault();
+    });
+    soltarImg2.on('drop', soltar);
+
+
+    soltarImg3.on('dragenter', function (event) {
+        event.preventDefault();
+    });
+    soltarImg3.on('dragover', function (event) {
+        event.preventDefault();
+    });
+    soltarImg3.on('drop', soltar);
+};
+
+function arrastrar(event) {
+    let elemento = event.target;
+    event.originalEvent.dataTransfer.setData('Text', elemento.getAttribute('id'));
+}
+async function soltar(event) {
+    event.preventDefault();
+    console.log(event)
+    let id = event.originalEvent.dataTransfer.getData('Text');
+    let imagen = $('#' + id)
+    imagen.css('display', 'none')
+    $(event.target).html('<img src="' + imagen.attr('src') + '" height="400px" width="275px">');
+    contador++;
+}
+function reiniciar() {
+    window.location.reload();
+}
+
+$(document).ready(function () {
+    inicio();
+})
